b506a75466cf50525baa7d125b39423204432990
Stephan Hesse <stephan@emliri.com>
2020-03-16 14:39:44 +0100

    set fragments-per-key to 1

--- exmg_key_system_test.sh
+++ exmg_key_system_test.sh
@@ -29,7 +29,7 @@ export output=../dash-out-test
 export FF_EXMG_KEY_FILE_OUT=$output/ # ending slash is mandatory (or empty string "")
 export FF_EXMG_KEYS_MQTT=1
 export FF_EXMG_MESSAGE_SEND_DELAY="5.0" # seconds
-#export FF_EXMG_KEYS_DRY_RUN=1
+export FF_EXMG_KEY_SCOPE_NB_OF_FRAGMENTS="1" # amount
 
 echo $output
 
