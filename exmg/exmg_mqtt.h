70078b967117882157adbf8381338b4d3ac617f6
Stephan Hesse <tchakabam@gmail.com>
2020-07-27 00:21:45 +0200

    exmg_mqtt: fix some bugs in new paho-lib wrappers

--- exmg/exmg_mqtt.h
+++ exmg/exmg_mqtt.h
@@ -16,7 +16,6 @@
 
 // MQTT config (defaults)
 #define EXMG_MQTT_URL "ssl://mqtt.liveryvideo.com:8883"
-
 #define EXMG_MQTT_CLIENTID "user2"
 #define EXMG_MQTT_USERNAME EXMG_MQTT_CLIENTID
 #define EXMG_MQTT_PASSWD "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyLWlkIjoidXNlcjIiLCJhdXRoLWdyb3VwcyI6InB1Ymxpc2g7c3Vic2NyaWJlIn0.BeBVt6WnpC9oNmd9-DLH7x4ROQaKUhQFOZYJBwyaV5GyYyXa3Hm-8GCppOFITp1-Djs5H5NrfThuDtDTaDr0vOZNPaSmkztW-fYWMZ7B4eUFEbnTwwoKzaZZRuQHqY_uFLyYM030VL3NMLhPdoyYnPrdHn-TnLHoZUkSh9gx0mBRJTA9fnurSgqRCM2ho4W5o_yQhB_ggIp04DgM0oZG8Qmts5nEmXLTTpEJs2wTla0aJ9a9bd2LBPF5jbXNkG8kI3BuH5-lq35EMH1UMMKBzqF4OiZ9pTc7GV9qhUDJvJOUlA3wxLWWLh4fuFQG-N90e_5Pj1xGNswIgAwzL7UehCBA03UFisY5AfNmoX0qQ_1Lbhl7xVnlMBtj4pSPCpQMHiuDkvQWvNFnmp9VCLcAQWvOXtasPl37Zd0Mwz1Nsn6ceUxBqUtCg26yA-v5Fs0nX5Z2UTCqtrLDjkNbtuMZTglkauVhZkvZp7ITC0bW_goNKSZgvRhGeh4cFkkUap059s7SUnrfWG7XMLoAsrG_nasfpNrHsHs2yZ7Hs8omYf7AkTP_vpXwNgBKfO5Y6wZyI50drTmZvVa1WXZp2YIbBSEz4rGa-lqAKVNKqMWK0g_3aUP6rQDbnenQRv7ZZ4x3W5QyYiUYD4PzkpWOSupIqgB968GTg_wrbPlGSysyR_U"
@@ -35,9 +34,6 @@ typedef struct ExmgMqttPubConfig {
 typedef struct ExmgMqttPubContext {
     ExmgMqttPubConfig config;
     MQTTClient client;
-    MQTTProperties pub_props;
-    MQTTClient_createOptions create_opts;
-    MQTTClient_nameValue* infos;
     const char *server_uri;
     int is_connected;
     int is_tls;
@@ -46,18 +42,17 @@ typedef struct ExmgMqttPubContext {
 static void exmg_mqtt_pub_context_init(ExmgMqttPubContext **ptr, ExmgMqttPubConfig config) {
 
     ExmgMqttPubContext *s = (ExmgMqttPubContext*) malloc(sizeof(ExmgMqttPubContext));
-
-    s->pub_props = (MQTTProperties) MQTTProperties_initializer;
-    s->create_opts = (MQTTClient_createOptions) MQTTClient_createOptions_initializer;
-    s->create_opts.MQTTVersion = MQTT_VERSION;
-    s->infos = MQTTClient_getVersionInfo();
-    s->server_uri = config.url;
+    s->config = config;
     s->is_tls = strncmp(config.url, "ssl://", 6) == 0 || strncmp(config.url, "wss://", 6) == 0;
+    s->server_uri = config.url;
+
+    MQTTClient_createOptions opts = MQTTClient_createOptions_initializer;
+    opts.MQTTVersion = MQTT_VERSION;
 
     int rc = MQTTClient_createWithOptions(&s->client, s->server_uri,
-        EXMG_MQTT_CLIENTID, 
+        config.client_id, 
             MQTTCLIENT_PERSISTENCE_NONE, NULL, // Q: what does this do/not do ?
-                &s->create_opts);
+                &opts);
 
     if (rc != MQTTCLIENT_SUCCESS)
     {
@@ -65,6 +60,8 @@ static void exmg_mqtt_pub_context_init(ExmgMqttPubContext **ptr, ExmgMqttPubConf
         free(s);
         return;
     }
+
+    av_log(NULL, AV_LOG_INFO, "Created MQTT publish context with client handle: %p / %p\n", s->client, &s->client);
 
     *ptr = s;
 }
@@ -97,9 +94,10 @@ static int exmg_mqtt_pub_connect(ExmgMqttPubContext *s)
     conn_opts.username = EXMG_MQTT_USERNAME;
     conn_opts.password = EXMG_MQTT_PASSWD;
     conn_opts.cleansession = 1;
+
+    MQTTClient_SSLOptions ssl_opts = MQTTClient_SSLOptions_initializer;
     if (s->is_tls)
     {
-        MQTTClient_SSLOptions ssl_opts = MQTTClient_SSLOptions_initializer;
         ssl_opts.verify = 1;
         ssl_opts.CApath = NULL;
         ssl_opts.keyStore = NULL;
@@ -109,13 +107,15 @@ static int exmg_mqtt_pub_connect(ExmgMqttPubContext *s)
         ssl_opts.enabledCipherSuites = NULL;
         conn_opts.ssl = &ssl_opts;
     }
-    
-    int rc = MQTTClient_connect(&s->client, &conn_opts);
+
+    av_log(NULL, AV_LOG_INFO, "MQTTClient_connect with handle: %p / %p\n", s->client, &s->client);
+
+    int rc = MQTTClient_connect(s->client, &conn_opts);
     if (rc == MQTTCLIENT_SUCCESS) {
         av_log(NULL, AV_LOG_INFO, "MQTTClient_connect success to: %s\n", s->server_uri);
         s->is_connected = 1;
     } else {
-        av_log(NULL, AV_LOG_ERROR, "MQTTClient_connect failed to: %s\n", s->server_uri);
+        av_log(NULL, AV_LOG_ERROR, "MQTTClient_connect failed (error-code=%d) to: %s\n ", rc, s->server_uri);
         s->is_connected = 0;
     }
     return s->is_connected;
@@ -123,14 +123,12 @@ static int exmg_mqtt_pub_connect(ExmgMqttPubContext *s)
 
 static int exmg_mqtt_pub_send(ExmgMqttPubContext *s, uint8_t* message, int message_length)
 {
-    MQTTClient *client = &s->client;
-
     if (!s->is_connected && exmg_mqtt_pub_connect(s) != MQTTCLIENT_SUCCESS) {
         av_log(NULL, AV_LOG_ERROR, "exmg_mqtt_client_connect failed\n");
         return 0;
     }
 
-    int rc = MQTTClient_publish(client, s->config.topic, message_length, message, 0, 0, NULL);
+    int rc = MQTTClient_publish(s->client, s->config.topic, message_length, message, 0, 0, NULL);
     if (rc != MQTTCLIENT_SUCCESS)
     {
         av_log(NULL, AV_LOG_ERROR, "MQTTClient_publish failed\n");
