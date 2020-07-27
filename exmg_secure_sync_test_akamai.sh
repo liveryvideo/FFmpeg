708ace859669f71137652f746c5866e1004dc146
Stephan Hesse <tchakabam@gmail.com>
2020-07-27 19:00:29 +0200

    Akamai test script: fix env vars

--- exmg_secure_sync_test_akamai.sh
+++ exmg_secure_sync_test_akamai.sh
@@ -31,13 +31,13 @@ export key_output="tmp"
 rm -Rf $key_output
 mkdir -p $key_output
 
-export FF_EXMG_SECURE_SYNC_ON=0
-export FF_EXMG_SECURE_SYNC_DRY_RUN=0
-export FF_EXMG_SECURE_SYNC_NO_ENCRYPTION=0 # any non-empty string -> true
+export FF_EXMG_SECURE_SYNC_ON=""
+#export FF_EXMG_SECURE_SYNC_DRY_RUN=""
+#export FF_EXMG_SECURE_SYNC_NO_ENCRYPTION="" 
 
-export FF_EXMG_SECURE_SYNC_MQTT_PUB=1
-export FF_EXMG_SECURE_SYNC_FS_PUB_BASEPATH=$key_output/ # ending slash is mandatory (or empty string "")
-export FF_EXMG_SECURE_SYNC_MESSAGE_SEND_DELAY="20" # seconds (float)
+export FF_EXMG_SECURE_SYNC_MQTT_PUB=1 # anything set will enable MQTT key-pub
+export FF_EXMG_SECURE_SYNC_FS_PUB_BASEPATH=$output/ # ending slash is mandatory (or empty string "") / unset to disable fs key-pub 
+export FF_EXMG_SECURE_SYNC_MESSAGE_SEND_DELAY="5" # seconds (float)
 export FF_EXMG_SECURE_SYNC_FRAGMENTS_PER_KEY="30" # amount (int)
 
 echo "Publishing to: $output and sub-directory: $sub_folder"
@@ -58,15 +58,12 @@ export log_level="info" # quiet / error / debug / verbose
               setpts='trunc(PTS/1K)*1K+st(1,trunc(RTCTIME/1K))-1K*trunc(ld(1)/1K)', \
               drawtext=rate=30:text='%{localtime}.%{eif\:1M*t-1K*trunc(t*1K)\:d}:' \
               x=300:y=300:fontfile=./Linebeam.ttf:fontsize=48:fontcolor='white':boxcolor=0x00AAAAAA:box=1" \
-       -c:v libx264 \
-       -preset medium \
        -b:v $video_bitrate \
        -s $output_resolution \
        -pix_fmt yuv420p \
        -sc_threshold 0 \
        -force_key_frames "expr:gte(t,n_forced*"$segment_size_in_seconds")" \
        -bf 0 \
-       -x264opts scenecut=-1:rc_lookahead=0 \
        -c:a aac \
        -b:a $audio_bitrate \
        -seg_duration $segment_size_in_seconds \
