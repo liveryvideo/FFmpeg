cb55c8135ec3ed852831e175eb68fcb0479d75cc
joepadmiraal <joep@groovytunes.nl>
2020-12-14 13:59:49 +0100

    Fix reading invalid data in http.c

--- libavformat/http.c
+++ libavformat/http.c
@@ -1731,8 +1731,11 @@ static int http_shutdown(URLContext *h, int flags)
             curr_time_ms = av_gettime() / 1000;
             req_time_ms = curr_time_ms - s->start_time_ms;
             av_log(h, AV_LOG_INFO, "HTTP response: %d, duration: %"PRId64", url: %s \n", s->http_code, req_time_ms, s->location);
-            if (s->http_code >= 400 || s->http_code == 0) {
+            if (s->http_code >= 400) {
                 av_log(h, AV_LOG_INFO, "HTTP response. url: %s, data: [%s]\n", s->location, buf);
+            }
+            if (s->http_code == 0) {
+                av_log(h, AV_LOG_INFO, "HTTP response. url: %s\n", s->location);
             }
 
             if (read_ret < 0 && read_ret != AVERROR(EAGAIN))
