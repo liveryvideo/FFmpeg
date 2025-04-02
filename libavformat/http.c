4f4a33b4a2b97faef679fd7dbc625ead3c36f8b5
Anton Maslennikov <anton@liveryvideo.nl>
2025-04-02 14:37:13 +0200

    Return accidently removed ff_http_get_code

--- libavformat/http.c
+++ libavformat/http.c
@@ -2259,6 +2259,11 @@ static int http_proxy_write(URLContext *h, const uint8_t *buf, int size)
     return ffurl_write(s->hd, buf, size);
 }
 
+int ff_http_get_code(URLContext *h) {
+    HTTPContext *s = h->priv_data;
+    return s->http_code;
+}
+
 const URLProtocol ff_httpproxy_protocol = {
     .name                = "httpproxy",
     .url_open            = http_proxy_open,
